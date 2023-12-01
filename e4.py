f8c8178904c484ab5c796d8376d05d4409f401fd
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-12-01 12:31:30 +0100

    ee

--- e4.py
+++ e4.py
@@ -3,3 +3,22 @@ Progarma que crea un tablero de escacs
 """
 BLANC = "⬜"
 NEGRE = "⬛"
+i = 0
+for h in range(0, 7):
+    for j in range(0, 7):
+
+        if j == 7:
+            if (j + h) % 2 == 0:
+                print(BLANC, end = "")
+            else:
+                print(NEGRE, end = "")
+
+        elif (j + h) % 2 == 0:
+            print(BLANC)
+        elif (j + h) % 2 != 0:
+            print(NEGRE)
+    print()
+
+
+
+
--- e5.py
+++ e5.py
@@ -1,2 +1,10 @@
+"""
 
+"""
+num1 = int(input("introduce el primer valor de la multiplicación"))
+num2 = int(input("introduce el segundo valor de la multiplicación"))
+res = 0
+for x in range(0, num1):
+    res = res + num2
+print(res)
 
