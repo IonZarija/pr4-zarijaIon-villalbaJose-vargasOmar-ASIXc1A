1170b084a34a1507dedab06eeb25ceb6ef6b3a0d
ion.zarija.7e5 <ion.zarija.7e5@itb.cat>
2023-12-01 13:05:34 +0100

    g

--- e2.py
+++ e2.py
@@ -2,7 +2,12 @@
 
 """
 h = int(input("Alçada?: "))
-print()
+y=1
 for i in range(0,h):
-    print(i+1, " "*(i-1), i+1 ,end="")
+    if i==h-1:
+        print(h*str(h), " "*(h), end="")
+    elif h>i>0:
+        print(i+1, " "*(i-1), i+1 ,end="")
+    elif i==0:
+        print(y*"1", end="")
     print()
