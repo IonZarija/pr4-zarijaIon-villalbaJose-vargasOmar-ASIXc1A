ee758029b5e2e4fd1fe3bfcc92fb2bc9a14f0cb3
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-12-01 11:45:23 +0100

    ee

--- e1.py
+++ e1.py
@@ -1,1 +1,18 @@
+"""
+
+"""
+num = 0
+null = 0
+pos = 0
+res = 0
+for x in range(0,9):
+    num = int(input("Introduce una número: "))
+    if num == 0:
+        null = null + 1
+    elif num > 0:
+        pos = pos + 1
+    elif num < 0:
+        res = res + 1
+print("Has introducido ",null, " zeros",",", pos, "números positivos, y finalmente, ",res," números negativos")
+
 
