edcf9275bb53024eacd55240a086e2e420326d91
joseantonio <jose.villalba.7e7@itb.cat>
2023-12-01 19:16:06 +0100

    aa

--- e4.py
+++ e4.py
@@ -6,11 +6,16 @@ NEGRE = "██"
 
 for h in range(0, 8):
     for j in range(0, 8):
-
-        if (j + h) % 2 != 0:
-            print(BLANC, end = "")
-        elif (j + h) % 2 == 0:
-            print(NEGRE, end = "")
+        if j == 0:
+            if (j + h) % 2 != 0:
+                print(8-h, BLANC, end="")
+            elif (j + h) % 2 == 0:
+                print(8-h, NEGRE, end="")
+        else:
+            if (j + h) % 2 != 0:
+                print(BLANC, end="")
+            elif (j + h) % 2 == 0:
+                print(NEGRE, end="")
     print()
 
 
