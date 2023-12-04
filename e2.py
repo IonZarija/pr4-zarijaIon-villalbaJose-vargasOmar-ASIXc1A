018b3ac188835fc7b79a0894dc28626e6399d1a0
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-12-04 12:48:43 +0100

    ee

--- e2.py
+++ e2.py
@@ -4,19 +4,22 @@ ASIXc 1A
 M03 UF1 Pr4
 Programa que hace una pirámide
 """
-h = int(input("Alçada?: "))
-while h >= 10:
+try:
     h = int(input("Alçada?: "))
-y=1
-for i in range(1,h+1):
-    if i == h:
-        for x in range(h):
-            print(i, sep=" ", end=" ")
-    elif h > i > 2:
-        print(i, y*" ", i, end ="")
-        y += 2
-    elif i == 1:
-        print(i, end="")
-    elif i == 2:
-        print(i, i, end="")
-    print()
+    while h >= 10 or h < 0:
+        h = int(input("Alçada?: "))
+    y=1
+    for i in range(1,h+1):
+        if i == h:
+            for x in range(h):
+                print(i, sep=" ", end=" ")
+        elif h > i > 2:
+            print(i, y*" ", i, end ="")
+            y += 2
+        elif i == 1:
+            print(i, end="")
+        elif i == 2:
+            print(i, i, end="")
+        print()
+except ValueError:
+    print("No has introducido un valor numerico")
