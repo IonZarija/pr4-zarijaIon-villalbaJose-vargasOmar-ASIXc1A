0642eafc92a90613f4a5efdc4ee4925ee8e41b43
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-12-04 12:26:52 +0100

    ee

--- e5.py
+++ e5.py
@@ -9,13 +9,13 @@ try:
     num2 = int(input("Introduce el segundo valor de la multiplicación"))
     res = 0
     while num1 < 0 or num2 < 0:
-        num1 = int(input("Introduce el primer valor positivo de la multiplicación"))
-        num2 = int(input("Introduce el segundo valor positivo de la multiplicación"))
+        num1 = int(input("Introduce el primer valor POSITIVO de la multiplicación"))
+        num2 = int(input("Introduce el segundo valor POSITIVO de la multiplicación"))
 
     for x in range(0, num1):
         res = res + num2
 
     print(res)
 except ValueError:
-    print("No has introducido un número natural")
+    print("No has introducido un carácter numérico")
 
