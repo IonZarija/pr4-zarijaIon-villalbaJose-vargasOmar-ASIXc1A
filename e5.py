33a3c994ff58e10f91ee944d9fde4ca637d417a5
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-12-04 12:44:04 +0100

    ee

--- e5.py
+++ e5.py
@@ -2,7 +2,7 @@
 Ion Zarija, Omar Vargas, José A. Villalba
 ASIXc 1A
 M03 UF1 Pr4
-Progama que al introducirle dos valores, devuelve su multiplicación
+Progama que al introducirle dos valores enteros, devuelve su multiplicación
 """
 try:
     num1 = int(input("Introduce el primer valor de la multiplicación"))
@@ -13,14 +13,13 @@ try:
         num1 = -num1
         num2 = -num2
         for x in range(0, num1):
-             res = res + num2
-    if num1 < 0:
+            res = res + num2
+    elif num1 < 0:
         for x in range(0, num2):
-             res = res + num1
+            res = res + num1
     elif num2 < 0:
         for x in range(0, num1):
-             res = res + num2
+            res = res + num2
     print(res)
 except ValueError:
     print("No has introducido un carácter numérico")
-
